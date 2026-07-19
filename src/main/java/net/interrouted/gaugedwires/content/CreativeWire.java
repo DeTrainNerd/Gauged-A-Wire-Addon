d59e8080f76bf85accdfcf7efce636920e743b84
DeTrainNerd <dwspelde3@gmail.com>
2026-07-19 17:52:16 -0500

    Added creative wire item texture

--- src/main/java/net/interrouted/gaugedwires/content/CreativeWire.java
+++ src/main/java/net/interrouted/gaugedwires/content/CreativeWire.java
@@ -1,12 +1,13 @@
 package net.interrouted.gaugedwires.content;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 
 public class CreativeWire extends Item {
 
     public CreativeWire() {
 
-        super(new Item.Properties());
+        super(new Item.Properties().rarity(Rarity.EPIC));
 
     }
 }
